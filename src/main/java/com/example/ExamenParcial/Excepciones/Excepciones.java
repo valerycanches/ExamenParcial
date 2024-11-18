a4d311e5d786f186b1ee1722319f425eee841721
valerycanches <74372171+valerycanches@users.noreply.github.com>
2024-11-18 16:51:23 -0500

    eliminando carpeta excepciones

--- src/main/java/com/example/ExamenParcial/Excepciones/Excepciones.java
+++
@@ -1,8 +0,0 @@
-package com.example.ExamenParcial.Excepciones;
-
-// Esta es la clase personalizada de excepciones
-public class Excepciones extends RuntimeException {
-    public Excepciones(String mensaje) {
-        super(mensaje); // Llama al constructor de RuntimeException
-    }
-}
